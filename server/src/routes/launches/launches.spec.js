9d5db0b69d9a260b90fd933719fae30b20a15c75
Joshua-Gaspar <joshuagaspar03@gmail.com>
2022-01-08 13:35:47 -0400

    load planet data in test

--- server/src/routes/launches/launches.spec.js
+++ server/src/routes/launches/launches.spec.js
@@ -1,10 +1,16 @@
 const request = require('supertest');
 const app = require('../../app');
+const { loadPlanetsData } = require('../../models/planets.model');
 const {mongoConnect , mongoDisconnect } =require('../../services/mongo');
+
+const {
+    loadPlanetsData,
+} = require('../../models/planets.model')
 
 describe('Launches API', () =>{
     beforeAll (async ()=>{
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll(async()=>{
